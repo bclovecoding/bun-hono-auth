6fda191c9186a92b85215791054a415a4ece3396
Benny Cao <benny.cao@copeland.com>
2025-08-15 16:27:37 +0800

    add queries test

--- src/db/queries.test.ts
+++ src/db/queries.test.ts
@@ -1,7 +1,7 @@
-import { Database } from 'bun:sqlite'
-import { describe, it, expect, beforeEach, afterEach } from 'bun:test'
-import { insertUser } from './queries'
+import { describe, expect, it, beforeEach, afterEach } from 'bun:test'
+import { getUserByEmail, insertUser } from './queries'
 import { createTestDb } from '../test/test-db'
+import { Database } from 'bun:sqlite'
 
 let db: Database
 
@@ -14,36 +14,53 @@ afterEach(() => {
 })
 
 describe('insertUser', () => {
-
-  it('should insert a user and return the user ID', async () => {
-    const email = 'test@example.com'
-    const password = 'super-secure-pa$$word'
-    const id = await insertUser(db, email, password)
-    expect(id).toBeDefined()
+  it('should insert a user into the database', async () => {
+    const email = 'test@test.com'
+    const password = 'password123'
+    const userId = await insertUser(db, email, password)
+    expect(userId).toBeDefined()
   })
 
-  it('should not insert a user with an existing email', async () => {
-    const email = 'test@example.com'
-    const password = 'super-secure-pa$$word'
+  it('should throw an error if the email is already in the db', async () => {
+    const email = 'test@test.com'
+    const password = 'password123'
     await insertUser(db, email, password)
+
     try {
       await insertUser(db, email, password)
     } catch (error) {
       expect(error).toBeInstanceOf(Error)
       // @ts-ignore
-      expect(error.message).toContain('UNIQUE constraint failed')
+      expect(error.message).toMatch(/UNIQUE constraint failed/)
     }
   })
 
   it('should throw an error if the password is empty', async () => {
-    const email = 'test@example.com'
+    const email = 'test@test.com'
     const password = ''
     try {
       await insertUser(db, email, password)
     } catch (error) {
       expect(error).toBeInstanceOf(Error)
       // @ts-ignore
-      expect(error.message).toContain('password must not be empty')
+      expect(error.message).toMatch(/password must not be empty/)
     }
+  })
+})
+
+describe('getUserByEmail', () => {
+  it('return a user by a given email', async () => {
+    const email = 'test@test.com'
+    const password = 'password123'
+    await insertUser(db, email, password)
+
+    const user = getUserByEmail(db, email)
+    expect(user).toBeDefined()
+  })
+
+  it('returns null when there is no user by that email', async () => {
+    const email = 'test@test.com'
+    const user = getUserByEmail(db, email)
+    expect(user).toBeNull()
   })
 })
